abfc993342ac4f325cf90ab064c7a42bfe1980b6
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-23 15:05:32 +0100

    feat: form

--- form.php
+++ form.php
@@ -6,6 +6,17 @@
     <title>php-badwords</title>
 </head>
 <body>
+    <h2>
+        FORM
+    </h2>
     
+    <form method="post" action="process.php">
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea name="paragraph" id="paragraph" cols="30" rows="10"></textarea><br>
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" id="word" name="word"><br><br>
+        <input type="submit" value="Invia">
+    </form>
+
 </body>
 </html>
